b3de2f06b2b42369c49df332d04a0a8fdce08242
briac ferte <you@example.com>
2020-10-23 18:02:54 +0200

    23/10/2020

---
+++ AppNettoyagePC/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppNettoyagePC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public DirectoryInfo winTemp;
+        public DirectoryInfo appTemp;
+        public MainWindow()
+        {
+            InitializeComponent();
+            winTemp = new DirectoryInfo(@"C:\Windows\Temp");
+            winTemp = new DirectoryInfo(System.IO.Path.GetTempPath());
+
+        }
+
+        //Calcul de la taille d'un dossier
+
+        public long DirSize(DirectoryInfo dir)
+        {
+            return dir.GetFiles().Sum(fi => fi.Length) + dir.GetDirectories().Sum(di => DirSize(di));
+        }
+
+        private void Button_MAJ_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Votre logiciel est à jour !","Mise à jour",MessageBoxButton.OK,MessageBoxImage.Information);
+        }
+
+        private void Button_Histo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO: Créer page historique", "Historique", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Web_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("https://github.com/bferte")
+                            {
+                                UseShellExecute = true
+                            });
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Erreure : " + ex.Message);
+            }
+            
+        }
+
+        private void Button_Analyser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
